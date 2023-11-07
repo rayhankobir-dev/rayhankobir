85354369af737a11968d32404543a5bb91533397
rayhankobir-dev <rayhankobir793@gmail.com>
2023-11-07 22:35:10 +0600

    new project added

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -58,4 +58,22 @@ const projects = [
       </>
     ),
   },
+  {
+    title: "Taskify-Task Management",
+    imgSrc: "https://taskify-main.vercel.app/screenshot.png",
+    code: "https://github.com/rayhankobir-dev/taskify",
+    projectLink: "https://taskify-main.vercel.app",
+    tech: ["React", "Node.js", "MongoDB", "Tailwind"],
+    description:
+      "A task management application built with react(context api), authentication and authorization. I've made backend api by using node.js and express with mongodb database.",
+    modalContent: (
+      <>
+        <p>
+          A task management application built with react(context api),
+          authentication and authorization. I have made backend api by using
+          node.js and express with mongodb database
+        </p>
+      </>
+    ),
+  },
 ];
